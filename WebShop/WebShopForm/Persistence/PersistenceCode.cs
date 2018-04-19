2f02d7c8b419cacdba4dff22aa51d4753f97ddc0
jonas bries <jonas.briers@hotmail.com>
2018-04-19 11:07:31 +0200

    Create GetProducts method in persistensecode with unfinished implementation

--- WebShop/WebShopForm/Persistence/PersistenceCode.cs
+++ WebShop/WebShopForm/Persistence/PersistenceCode.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebShopForm.Business;
+using MySql.Data.MySqlClient;
 
 namespace WebShopForm.Persistence
 {
     public class PersistenceCode
     {
         string connStr = "server=localhost;database=dbshop;password=Test123;user id=root";
+
+        public List<Product> GetProducts()
+        {
+            var connection = new MySqlConnection(connStr);
+            connection.Open();
+            string querryStr = "select * from tblproducts";
+            var command = new MySqlCommand(querryStr, connection);
+            var querryOutput = command.ExecuteReader();
+            while(querryOutput.Read())
+            {
+
+            }
+
+
+            connection.Close();
+        }
     }
 }
