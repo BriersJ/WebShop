e21495d86f2720d1d460a6e2f820f506efe5fb58
jonas bries <jonas.briers@hotmail.com>
2018-04-19 10:52:22 +0200

    Add PersistenceCode class and connectionstring

---
+++ WebShop/WebShopForm/Persistence/PersistenceCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShopForm.Persistence
+{
+    public class PersistenceCode
+    {
+        string connStr = "server=localhost;database=dbwebshop;password=Test123;user id=root";
+    }
+}
