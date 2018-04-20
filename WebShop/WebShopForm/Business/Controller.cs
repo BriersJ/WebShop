413f0619c7f8f579ed0f545486f8347a5f7d3c6b
jonas bries <jonas.briers@hotmail.com>
2018-04-20 11:58:50 +0200

    Add unit test for controller

--- WebShop/WebShopForm/Business/Controller.cs
+++ WebShop/WebShopForm/Business/Controller.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebShopForm.Persistence;
 
 namespace WebShopForm.Business
 {
     public class Controller
     {
+        PersistenceCode persistenceCode = new PersistenceCode();
+
+        public List<Product> GetProducts()
+        {
+            return persistenceCode.GetProducts();
+        }
     }
 }
---
+++ WebShop/WebShopTest/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebShopForm.Business;
+
+namespace WebShopTest
+{
+    [TestClass]
+    public class TestController
+    {
+        private Controller controller;
+
+        [TestInitialize]
+        public void MakeController()
+        {
+            controller = new Controller();
+        }
+
+        [TestMethod]
+        public void GetProducts_NonEmptyDatabase_Success()
+        {
+            var products = controller.GetProducts();
+            Assert.IsTrue(products.Capacity >= 1);
+        }
+    }
+}
