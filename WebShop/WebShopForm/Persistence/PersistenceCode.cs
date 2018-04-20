e32e2bbd25afabceed0b10de617eede9e1700931
jonas bries <jonas.briers@hotmail.com>
2018-04-20 11:51:24 +0200

    Finish implementation of getproducts mathod

--- WebShop/WebShopForm/Persistence/PersistenceCode.cs
+++ WebShop/WebShopForm/Persistence/PersistenceCode.cs
@@ -13,18 +13,28 @@ namespace WebShopForm.Persistence
 
         public List<Product> GetProducts()
         {
+            var productList = new List<Product>();
             var connection = new MySqlConnection(connStr);
             connection.Open();
             string querryStr = "select * from tblproducts";
             var command = new MySqlCommand(querryStr, connection);
             var querryOutput = command.ExecuteReader();
-            while(querryOutput.Read())
+            while (querryOutput.Read())
             {
-
+                var product = new Product()
+                {
+                    ID = Convert.ToInt32(querryOutput["ID"]),
+                    Name = Convert.ToString(querryOutput["Name"]),
+                    Picture = Convert.ToString(querryOutput["Picture"]),
+                    Price = Convert.ToDouble(querryOutput["Price"]),
+                    Stock = Convert.ToInt32(querryOutput["Stock"])
+                };
+                productList.Add(product);
             }
 
 
             connection.Close();
+            return productList;
         }
     }
 }
